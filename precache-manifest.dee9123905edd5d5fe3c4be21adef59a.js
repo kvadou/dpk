61e8ea2d9bb18ecd7d75e9dc08101960ecf85d6a
Doug Kvamme <dougkvamme@gmail.com>
2023-03-08 14:01:05 -0600

    Updates

---
+++ precache-manifest.dee9123905edd5d5fe3c4be21adef59a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b2d2673319e580772f65e9f48d6b46",
+    "url": "/dpk/index.html"
+  },
+  {
+    "revision": "323824ee3bc2f5c78550",
+    "url": "/dpk/static/css/main.f77d7a38.chunk.css"
+  },
+  {
+    "revision": "f0585afd58bab4c12498",
+    "url": "/dpk/static/js/2.9a2180c5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/dpk/static/js/2.9a2180c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "323824ee3bc2f5c78550",
+    "url": "/dpk/static/js/main.66efadb4.chunk.js"
+  },
+  {
+    "revision": "354c8b7b32c45d31e087",
+    "url": "/dpk/static/js/runtime-main.d3eef330.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/dpk/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "b9d7d448a09d6aacb45d21aab2c6fd53",
+    "url": "/dpk/static/media/your photo.b9d7d448.png"
+  }
+]);
